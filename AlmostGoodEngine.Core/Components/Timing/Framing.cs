cde9c03449e94f2738cf33200c5822048ce24460
Akadream <lortet.guillaume@gmail.com>
2024-04-15 22:40:14 +0200

    feature: framing system, procedural animations using second order dynamics

---
+++ AlmostGoodEngine.Core/Components/Timing/Framing.cs
@@ -0,0 +1,111 @@
+﻿using AlmostGoodEngine.Core.ECS;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+
+namespace AlmostGoodEngine.Core.Components.Timing
+{
+	public class Framing : Component
+	{
+		/// <summary>
+		/// The current frame
+		/// </summary>
+		public int Frame { get; private set; }
+
+		/// <summary>
+		/// If the component may work inside the fixed time step update
+		/// </summary>
+		public bool IsPhysics { get; private set; }
+
+		/// <summary>
+		/// True is the framing component is currently running
+		/// </summary>
+		public bool IsRunning { get; private set; }
+
+		/// <summary>
+		/// The number of frames maximum computed
+		/// </summary>
+		public int Max { get; private set; }
+
+		/// <summary>
+		/// A dictionary containing actions the framing system will execute
+		/// </summary>
+		public Dictionary<int, Action> Actions { get; private set; }
+
+		public Framing(int max)
+		{
+			Max = max;
+		}
+
+		/// <summary>
+		/// Launch the framing system
+		/// </summary>
+		/// <param name="restart"></param>
+		public void Launch(bool restart = true)
+		{
+			IsRunning = true;
+
+			if (restart)
+			{
+				Frame = 0;
+			}
+		}
+
+		/// <summary>
+		/// Stop the framing system
+		/// </summary>
+		/// <param name="reset"></param>
+		public void Stop(bool reset = true)
+		{
+			IsRunning = false;
+			
+			if (reset)
+			{
+				Frame = 0;
+			}
+		}
+
+		public override void Update(GameTime gameTime)
+		{
+			base.Update(gameTime);
+
+			if (!IsRunning || IsPhysics)
+			{
+				return;
+			}
+
+			Increment();
+		}
+
+		public override void FixedUpdate(GameTime gameTime)
+		{
+			base.FixedUpdate(gameTime);
+
+			if (!IsRunning || !IsPhysics)
+			{
+				return;
+			}
+
+			Increment();
+		}
+
+		/// <summary>
+		/// Execute the next frame
+		/// </summary>
+		private void Increment()
+		{
+			Frame++;
+
+			// Check if an action is binded to the specific frame
+			if (Actions.TryGetValue(Frame, out Action action))
+			{
+				action();
+			}
+
+			if (Frame >= Max)
+			{
+				Stop();
+			}
+		}
+	}
+}
---
+++ AlmostGoodEngine.Core/Utils/SecondOrderDynamics.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+
+namespace AlmostGoodEngine.Core.Utils
+{
+	public class SecondOrderDynamics
+	{
+		public Vector2 XP { get; private set; }
+		public Vector2 Y { get; private set; }
+		public Vector2 YD { get; private set; }
+
+		public float K1 { get; private set; }
+		public float K2 { get; private set; }
+		public float K3 { get; private set; }
+		
+		public SecondOrderDynamics(float f, float z, float r, Vector2 x0)
+		{
+			K1 = z / (MathF.PI * f);
+			K2 = 1 / ((2 * MathF.PI * f) * (2 * MathF.PI * f));
+			K3 = r * z / (2 * MathF.PI * f);
+
+			XP = x0;
+			Y = x0;
+			YD = Vector2.Zero;
+		}
+
+		public Vector2 Update(float t, Vector2 x, Vector2 xd)
+		{
+			if (xd == Vector2.Zero)
+			{
+				xd = (x - XP) / t;
+				XP = x;
+			}
+
+			Y += t * YD;
+			YD += t * (x + K3 * xd - Y - K1 * YD) / K2;
+			return Y;
+		}
+	}
+}
